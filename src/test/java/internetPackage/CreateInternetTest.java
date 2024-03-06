e0e7c85bc170f5a63d7bd5e32dce96ecd24aee91
new <new@DESKTOP-7TDIRJN>
2024-03-06 17:57:58 +0530

    check it

---
+++ src/test/java/internetPackage/CreateInternetTest.java
@@ -0,0 +1,15 @@
+package internetPackage;
+
+import org.testng.annotations.Test;
+
+public class CreateInternetTest {
+	@Test(groups = "smoke")
+	public void praticeTest()
+	{
+		System.out.println("----Internet pass-------");
+	}
+
+}
+
+
+
---
+++ src/test/java/internetPackage/deleteInternet.java
@@ -0,0 +1,18 @@
+package internetPackage;
+
+import org.testng.annotations.Test;
+
+public class deleteInternet {
+	
+	public class deleteTest {
+		@Test(groups = "smoke")
+		public void praticeTest()
+		{
+			System.out.println("----delete pass-------");
+		}
+
+	}
+
+
+
+}
