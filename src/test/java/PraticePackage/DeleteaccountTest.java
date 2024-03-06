0d74c318dd6fd4b8c3532088ea6242339c7fd7d8
new <new@DESKTOP-7TDIRJN>
2024-03-06 13:52:44 +0530

    TestScript created

---
+++ src/test/java/PraticePackage/DeleteaccountTest.java
@@ -0,0 +1,17 @@
+package PraticePackage;
+
+import org.testng.annotations.Test;
+
+public class DeleteaccountTest {
+	
+	public class DemoPraticeTest {
+
+		@Test(groups = "smoke")
+		public void praticeTest()
+		{
+			System.out.println("----Deleted pass-------");
+		}
+
+
+}
+}
---
+++ src/test/java/PraticePackage/createaccountTest.java
@@ -0,0 +1,17 @@
+package PraticePackage;
+
+import org.testng.annotations.Test;
+
+public class createaccountTest {
+
+	
+	public class DemoPraticeTest {
+
+		@Test(groups = "smoke")
+		public void praticeTest()
+		{
+			System.out.println("----createaccount pass------");
+		}
+
+	}
+}
