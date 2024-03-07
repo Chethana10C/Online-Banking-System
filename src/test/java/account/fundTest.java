92350d11b64df6c47100969ab80117b4d63f28a1
Chethana10C <chethanack10@gmail.com>
2024-03-07 17:43:31 +0530

    Fund Done

---
+++ src/test/java/account/fundTest.java
@@ -0,0 +1,15 @@
+package account;
+
+import org.testng.annotations.Test;
+
+public class fundTest {
+	
+	@Test
+	public void praticeTest()
+	{
+		System.out.println("----fund pass-------");
+	}
+
+}
+
+
