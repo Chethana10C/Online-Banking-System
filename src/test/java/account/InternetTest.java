3fa69d280ad2ffbcb478b641463e54d79f1fdc4f
Chethana10C <chethanack10@gmail.com>
2024-03-08 10:14:21 +0530

    internet created

---
+++ src/test/java/account/InternetTest.java
@@ -0,0 +1,15 @@
+package account;
+
+import org.testng.annotations.Test;
+
+public class InternetTest {
+	
+	@Test(groups ="regression")
+	public void praticeTest()
+	{
+		System.out.println("----Internet pass-------");
+	}
+
+}
+
+
