749c0e18e34b776bc0cbe387b1f06c13e58f61fd
Chethana10C <chethanack10@gmail.com>
2024-03-08 10:02:12 +0530

    created

---
+++ src/test/java/account/createAccTest.java
@@ -0,0 +1,16 @@
+package account;
+
+import org.testng.annotations.Test;
+
+public class createAccTest {
+	
+	@Test(groups ="smokesuite, regression")
+	public void praticeTest()
+	{
+		System.out.println("----create pass-------");
+	}
+
+}
+	
+
+
