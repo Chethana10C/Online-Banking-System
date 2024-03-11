70aec126e065cc40b97d128f82c3277431702638
Chethana10C <chethanack10@gmail.com>
2024-03-11 19:04:45 +0530

    login Internet sucessfully

---
+++ src/test/java/internetPackage/loginternet.java
@@ -0,0 +1,13 @@
+package internetPackage;
+
+import org.testng.annotations.Test;
+
+public class loginternet {
+
+	@Test(groups = "smoke")
+	public void praticeTest()
+	{
+		System.out.println("----login sucessfully-------");
+	}
+
+}
